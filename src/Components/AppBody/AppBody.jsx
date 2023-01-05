96a8c3030a2f3890c2279404f5032aec7b22cead
Ankur982 <singh999ankur@gmail.com>
2023-01-06 04:25:42 +0530

    adding appBOdy media

--- src/Components/AppBody/AppBody.jsx
+++ src/Components/AppBody/AppBody.jsx
@@ -1,39 +1,47 @@
 import React from "react";
 import styled from "./AppBody.module.css";
 import { IoIosArrowForward } from "react-icons/io";
+import { IoArrowBack } from "react-icons/io5";
 import Referral from "../Referral/Referral";
 import Cards from "../Cards/Cards";
 import { Link } from "react-router-dom";
 
 const AppBody = () => {
-	return (
-		<div className={styled.container}>
-			{/* Refer And Earn starts*/}
-			<div className={styled.uiux}>
-				<div>UI/UX</div>
-				<div className={styled.icons}>
-					<IoIosArrowForward />
-				</div>
-				<Link to={"/referandearn"}>
-					<div>Refer & Earn</div>
-				</Link>
-				<div className={styled.icons}>
-					<IoIosArrowForward />
-				</div>
-				<Link to={"/"}>
-					<div>Friends Referred</div>
-				</Link>
-			</div>
+  return (
+    <div className={styled.container}>
+      {/* Refer And Earn starts*/}
+      <div className={styled.uiux}>
+        <div>UI/UX</div>
+        <div className={styled.icons}>
+          <IoIosArrowForward />
+        </div>
+        <Link to={"/referandearn"}>
+          <div>Refer & Earn</div>
+        </Link>
+        <div className={styled.icons}>
+          <IoIosArrowForward />
+        </div>
+        <Link to={"/"}>
+          <div>Friends Referred</div>
+        </Link>
+      </div>
 
-			{/* Refer And Earn ends*/}
+      <Link to={"/"}>
+        <div className={styled.goback}>
+          <IoArrowBack />
+          <div>go back</div>
+        </div>
+      </Link>
 
-			{/* Your Referal Code div*/}
-			<Referral />
+      {/* Refer And Earn ends*/}
 
-			{/* friends who enrolled cards */}
-			<Cards />
-		</div>
-	);
+      {/* Your Referal Code div*/}
+      <Referral />
+
+      {/* friends who enrolled cards */}
+      <Cards />
+    </div>
+  );
 };
 
 export default AppBody;
